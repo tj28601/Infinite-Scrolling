d1f4184265982c287ce3a989aa2ee1331e63a568
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-08-14 13:27:12 -0400

    improved component code readability

--- app/javascript/react/containers/ArticleList.js
+++ app/javascript/react/containers/ArticleList.js
@@ -25,7 +25,7 @@ class ArticleList extends Component {
   handleScroll = (e) => {
     const { scrolling, totalArticles, offset } = this.state
     if (scrolling) return
-    if (totalArticles <= offset) return
+    // if (totalArticles <= offset) return
     const lastLi = document.querySelector('ol.articles > li:last-child')
     const lastLiOffset = lastLi.offsetTop + lastLi.clientHeight
     const pageOffset = window.pageYOffset + window.innerHeight
@@ -62,8 +62,10 @@ class ArticleList extends Component {
       offset: prevState.offset + 10,
       scrolling: true,
     }), this.loadArticles)
-    // console.log(this.state.limit)
-    // console.log(this.state.offset)
+    console.log(this.state.limit)
+    console.log(this.state.offset)
+    console.log(this.state.totalArticles)
+    console.log(this.state.scrolling)
   }
 
   render() {
@@ -71,7 +73,10 @@ class ArticleList extends Component {
     <ol className="articles">
     {
       this.state.articles.map(article => <li key={article.id}>
-      <ArticleTile {...article} />
+      <ArticleTile
+        title={ article.title }
+        published={ article.published }
+       />
     </li>)
     }
     </ol>
